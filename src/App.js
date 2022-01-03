89ca8611de21460526a8cd8341df223e24c223e0
veerav_eera <rajuveera2003@gmail.com>
2022-01-03 16:29:57 +0800

    changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Home from './Pages/Index';
+import Home from './Pages/index.js';
+
 function App() {
   return (
     <>
